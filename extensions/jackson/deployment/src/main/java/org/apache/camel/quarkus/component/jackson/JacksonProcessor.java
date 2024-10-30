01d65fcbcc0f8fabe633046463e2db513c1abe70
JiriOndrusek <ondrusek.jiri@gmail.com>
2024-10-30 11:12:44 +0100

    Revert "CEQ-10314: Disable combineUnicodeSurrogatres feature if COMBINE_UNICODE_SURROGATES_IN_UTF8 enum is not present in Jackson"
    This reverts commit 7b2c4d8bd8e6d7b7cffdff9fef10ddaa1f008214.

--- extensions/jackson/deployment/src/main/java/org/apache/camel/quarkus/component/jackson/JacksonProcessor.java
+++ extensions/jackson/deployment/src/main/java/org/apache/camel/quarkus/component/jackson/JacksonProcessor.java
@@ -17,21 +17,12 @@
 package org.apache.camel.quarkus.component.jackson;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.function.BooleanSupplier;
 
-import com.fasterxml.jackson.core.json.JsonWriteFeature;
 import io.quarkus.bootstrap.classloading.QuarkusClassLoader;
 import io.quarkus.deployment.annotations.BuildStep;
-import io.quarkus.deployment.builditem.BytecodeTransformerBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
-import io.quarkus.gizmo.Gizmo;
-import org.apache.camel.component.jackson.AbstractJacksonDataFormat;
-import org.objectweb.asm.ClassVisitor;
-import org.objectweb.asm.MethodVisitor;
-import org.objectweb.asm.Opcodes;
 
 public class JacksonProcessor {
 
@@ -54,54 +45,5 @@ public class JacksonProcessor {
             items.add(ReflectiveClassBuildItem.builder("org.joda.time.DateTime").build());
         }
         return items;
-    }
-
-    @BuildStep(onlyIf = JacksonCombineUnicodeSurrogatesAbsent.class)
-    BytecodeTransformerBuildItem transformMethodConfigureCombineUnicodeSurrogates() {
-        String message = "The combineUnicodeSurrogates option is not supported on Camel Quarkus";
-
-        // Rewrites AbstractJacksonDataFormat.configureCombineUnicodeSurrogates to:
-        //
-        // protected ObjectWriter configureCombineUnicodeSurrogates(ObjectWriter objectWriter) {
-        //    throw new IllegalStateException("The combineUnicodeSurrogates option is not supported on Camel Quarkus");
-        // }
-        return new BytecodeTransformerBuildItem.Builder()
-                .setClassToTransform(AbstractJacksonDataFormat.class.getName())
-                .setCacheable(true)
-                .setVisitorFunction((className, classVisitor) -> {
-                    return new ClassVisitor(Gizmo.ASM_API_VERSION, classVisitor) {
-                        @Override
-                        public MethodVisitor visitMethod(int access, String name, String descriptor, String signature,
-                                String[] exceptions) {
-                            MethodVisitor methodVisitor = super.visitMethod(access, name, descriptor, signature, exceptions);
-                            if (name.equals("configureCombineUnicodeSurrogates")) {
-                                return new MethodVisitor(Gizmo.ASM_API_VERSION, methodVisitor) {
-                                    @Override
-                                    public void visitCode() {
-                                        super.visitCode();
-                                        mv.visitLdcInsn(message);
-                                        mv.visitTypeInsn(Opcodes.NEW, "java/lang/IllegalStateException");
-                                        mv.visitInsn(Opcodes.DUP);
-                                        mv.visitLdcInsn(message);
-                                        mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/IllegalStateException",
-                                                "<init>", "(Ljava/lang/String;)V", false);
-                                        mv.visitInsn(Opcodes.ATHROW);
-                                    }
-                                };
-                            }
-                            return methodVisitor;
-                        }
-                    };
-                })
-                .build();
-    }
-
-    static final class JacksonCombineUnicodeSurrogatesAbsent implements BooleanSupplier {
-        @Override
-        public boolean getAsBoolean() {
-            JsonWriteFeature[] enumConstants = JsonWriteFeature.class.getEnumConstants();
-            return Arrays.stream(enumConstants)
-                    .noneMatch(enumEntry -> enumEntry.toString().equals("COMBINE_UNICODE_SURROGATES_IN_UTF8"));
-        }
     }
 }
